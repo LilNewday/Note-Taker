d4dca76aa8825493f42f656504ad5f899dc62946
LilNewday <lilnewday@gmail.com>
2023-04-17 23:16:06 -0400

    Created the routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,16 @@
+// dependencies
+const path = require("path");
+
+// routing
+module.exports = (app) => {
+  // creating routes
+  // notes.html file.
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // index.html file.
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
